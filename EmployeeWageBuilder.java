5f31dc901427ceaf8c5fe012fd985d1663353cb4
unknown <miyashaheen1995@gmail.com>
2021-03-30 23:05:58 +0530

    Modified:Code to calculate wages till total work hrs & days is reached

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,14 +5,16 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program !");
 		//variables
 		int empWorkHours=0;
-		int empWage=0,totalEmpWage=0;
+		int totalEmpWage=0,totalEmpWorkingHours=0,totalWorkingDays=0;
 		//Computation
-		for (int day=1; day<=NUM_OF_WORKING_DAYS; day++) {
+		while(totalEmpWorkingHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empCheck=(int)Math.floor(Math.random()*10)%3;
 			switch (empCheck) {
 				case IS_PART_TIME: empWorkHours=4;
@@ -22,11 +24,10 @@ public class EmployeeWageBuilder {
 				default : empWorkHours=0;
 							break;
 			}
-			empWage=empWorkHours * EMP_RATE_PER_HOUR;
-			totalEmpWage=totalEmpWage+empWage;
-			System.out.println("Employee Wage Day"+day+" :- "+empWage);
+			totalEmpWorkingHours += empWorkHours;
+			System.out.println("Day#:"+totalWorkingDays + "  Employee Work Hours:"+empWorkHours);	
 		}
-		
+		totalEmpWage=totalEmpWorkingHours * EMP_RATE_PER_HOUR;
 		System.out.println("Total Employee Wage :- "+totalEmpWage);	
 	}
 
