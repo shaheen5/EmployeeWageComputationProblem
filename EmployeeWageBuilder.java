b34e895026cbbede5645d4e1c874252a86b3dc2b
unknown <miyashaheen1995@gmail.com>
2021-04-03 01:00:36 +0530

    Modified : Code to compute emp wage for multiple companies

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,35 +3,34 @@ package EmployeeWageProblem;
 public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
 	
-	public static void computeEmpWage() {
+	public static void computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
 		//variables
-				int empWorkHours=0;
-				int totalEmpWage=0,totalEmpWorkingHours=0,totalWorkingDays=0;
-				//Computation
-				while(totalEmpWorkingHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-					totalWorkingDays++;
-					int empCheck=(int)Math.floor(Math.random()*10)%3;
-					switch (empCheck) {
+		int empWorkHours=0;
+		int totalEmpWage=0,totalEmpWorkingHours=0,totalWorkingDays=0;
+		//Computation
+		while(totalEmpWorkingHours <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+				totalWorkingDays++;
+				int empCheck=(int)Math.floor(Math.random()*10)%3;
+				switch (empCheck) {
 						case IS_PART_TIME: empWorkHours=4;
 											break;
 						case IS_FULL_TIME: empWorkHours=8;
 											break;
 						default : empWorkHours=0;
 									break;
-					}
+				}
 					totalEmpWorkingHours += empWorkHours;
 					System.out.println("Day#:"+totalWorkingDays + "  Employee Work Hours:"+empWorkHours);	
-				}
-				totalEmpWage=totalEmpWorkingHours * EMP_RATE_PER_HOUR;
-				System.out.println("Total Employee Wage :- "+totalEmpWage);	
-			}
+		}
+				totalEmpWage=totalEmpWorkingHours * empRatePerHour;
+				System.out.println("Total Employee Wage for Company: "+company+" is: "+totalEmpWage);	
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program !");
-		computeEmpWage();
+		computeEmpWage("Dmart",20,20,100);
+		computeEmpWage("Reliance",10,25,120);
+		computeEmpWage("Samsung",15,20,80);
 	}
 }
