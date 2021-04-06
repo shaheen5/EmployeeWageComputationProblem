edbd5642b651b6cdb6b18584af457dbdc0bb0f56
shaheen5 <miyashaheen1995@gmail.com>
2021-04-06 11:06:02 +0530

    Modified : Code to create objects & save totalWage for each company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,11 +3,22 @@ package EmployeeWageProblem;
 public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
+	private  final String company ;
+	private  final int empRatePerHour;
+	private  final int numOfWorkingDays;
+	private  final int maxHoursPerMonth;
+	private int totalEmpWage;
 	
-	public static void computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+	public EmployeeWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeEmpWage() {
 		//variables
-		int empWorkHours=0;
-		int totalEmpWage=0,totalEmpWorkingHours=0,totalWorkingDays=0;
+		int empWorkHours=0,totalEmpWorkingHours=0,totalWorkingDays=0;
 		//Computation
 		while(totalEmpWorkingHours <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 				totalWorkingDays++;
@@ -20,17 +31,27 @@ public class EmployeeWageBuilder {
 						default : empWorkHours=0;
 									break;
 				}
-					totalEmpWorkingHours += empWorkHours;
-					System.out.println("Day#:"+totalWorkingDays + "  Employee Work Hours:"+empWorkHours);	
+				totalEmpWorkingHours += empWorkHours;
+				System.out.println("Day#:"+totalWorkingDays + "  Employee Work Hours:"+empWorkHours);	
 		}
-				totalEmpWage=totalEmpWorkingHours * empRatePerHour;
-				System.out.println("Total Employee Wage for Company: "+company+" is: "+totalEmpWage);	
+		totalEmpWage=totalEmpWorkingHours * empRatePerHour;	
+	}
+	
+	@Override
+	public String toString() {
+		return "Total Employee Wage for company :"+company+" is: "+totalEmpWage;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program !");
-		computeEmpWage("Dmart",20,20,100);
-		computeEmpWage("Reliance",10,25,120);
-		computeEmpWage("Samsung",15,20,80);
+		EmployeeWageBuilder	dmart=new EmployeeWageBuilder("Dmart",20,20,100);
+		dmart.computeEmpWage();
+		System.out.println(dmart);
+		EmployeeWageBuilder	reliance=new EmployeeWageBuilder("Reliance",10,25,120);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
+		EmployeeWageBuilder	samsung=new EmployeeWageBuilder("Samsung",15,20,80);
+		samsung.computeEmpWage();
+		System.out.println(samsung);
 	}
 }
