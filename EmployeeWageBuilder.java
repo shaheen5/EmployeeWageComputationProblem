914b9f5e795d94b722c42e0d5142c12c0629567d
shaheen5 <miyashaheen1995@gmail.com>
2021-04-08 15:37:23 +0530

    Modified: Code to Store Daily Wage Along With TotalWage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,6 @@
 package EmployeeWageProblem;
 import java.util.ArrayList;
+import java.util.HashMap;
 
   interface IComputeEmpWage{
 	   public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 	   public  final int numOfWorkingDays;
 	   public  final int maxHoursPerMonth;
 	   public int totalEmpWage;
+	   public int dailyWage;
 
 	   public CompanyEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		   this.company = company;
@@ -21,10 +23,14 @@ import java.util.ArrayList;
 	   }
 
 	   public void setTotalEmpWage(int totalEmpWage) {
-		this.totalEmpWage=totalEmpWage;
+		   this.totalEmpWage=totalEmpWage;
 	   }
+	  
+       public void setDailyWage(int dailyWage) {
+    	   this.dailyWage = dailyWage;
+       }
 
-	   @Override
+	@Override
 	   public String toString() {
 		   return "Total Employee Wage for Company "+company+" is: "+totalEmpWage+"\n";
 	   }
@@ -34,14 +40,17 @@ import java.util.ArrayList;
   	   public static final int IS_PART_TIME=1;
        public static final int IS_FULL_TIME=2;
 	   private ArrayList<CompanyEmployeeWage> companyEmpWageList;
+	   private HashMap<String,CompanyEmployeeWage> companyToEmpWageMap;
 
        public EmployeeWageBuilder() {
 		 companyEmpWageList = new ArrayList<>();
+		 companyToEmpWageMap=new HashMap<>();
        }
 
 	   public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
 		 CompanyEmployeeWage companyEmpWage=new CompanyEmployeeWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
 		 companyEmpWageList.add(companyEmpWage);
+		 companyToEmpWageMap.put(company, companyEmpWage);
 	   }
 
 	   public void computeEmpWage() {
@@ -54,7 +63,7 @@ import java.util.ArrayList;
 
 	   private int computeEmpWage(CompanyEmployeeWage companyEmpWage) {
 		 //variables
-		 int empWorkHours=0,totalEmpWorkingHours=0,totalWorkingDays=0;
+		 int empWorkHours=0,totalEmpWorkingHours=0,totalWorkingDays=0,dailyWage=0;
 		 //Computation
 	   	 while(totalEmpWorkingHours <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
 		 	totalWorkingDays++;
@@ -63,19 +72,20 @@ import java.util.ArrayList;
 				case IS_PART_TIME: empWorkHours=4;
 			             		   break;
 				case IS_FULL_TIME: empWorkHours=8;
-						   break;
+						           break;
 				default : empWorkHours=0;
 	             		 	  break;
 			}
 	   	 totalEmpWorkingHours += empWorkHours;
-		 System.out.println("Day#:"+totalWorkingDays + "  Employee Work Hours:"+empWorkHours);	
+	   	 dailyWage=empWorkHours*companyEmpWage.empRatePerHour;
+		 System.out.println("Day#:"+totalWorkingDays + "  Employee Work Hours:"+empWorkHours+"  Daily Wage :"+dailyWage);	
 		 }
 		 return totalEmpWorkingHours * companyEmpWage.empRatePerHour;	
 	   }
 
 	   public static void main(String[] args) {
 		 System.out.println("Welcome to Employee Wage Computation Program !");
-		 EmployeeWageBuilder	empWageBuilder=new EmployeeWageBuilder();
+		 IComputeEmpWage empWageBuilder=new EmployeeWageBuilder();
 		 empWageBuilder.addCompanyEmpWage("Dmart",20,20,100);
 		 empWageBuilder.addCompanyEmpWage("Reliance",10,25,120);
 		 empWageBuilder.addCompanyEmpWage("Samsung",15,20,80);
